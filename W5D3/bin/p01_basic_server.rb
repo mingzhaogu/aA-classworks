5defc04d80aed5fcd865fd1d777af30ec956d09f
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-03-14 15:41:52 -0700

    Complete p01

--- W5D3/bin/p01_basic_server.rb
+++ W5D3/bin/p01_basic_server.rb
@@ -1,1 +1,20 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+
+  if req.path == "/"
+    res.write("Hello world!")
+  else
+    res.write("#{req.path}")
+  end
+
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
