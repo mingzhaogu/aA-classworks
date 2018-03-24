017a57fc5ee72d9845eac39511184ec174a5db9a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-03-24 12:43:17 -0700

    Update controller_base and session

--- W5D3/lib/session.rb
+++ W5D3/lib/session.rb
@@ -4,16 +4,24 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @req = req
+    cookie_value = Rack::Request.cookies['_rails_lite_app']
+    @cookie = cookie_value ? JSON.parse(cookie_value) : Hash.new {[]}
   end
 
   def [](key)
+    @cookie[key.to_sym]
   end
 
   def []=(key, val)
+    @cookie[key.to_sym] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    @cookie[:path] = "/"
+    @cookie = @cookie.to_json
+    Rails::Response.set_cookie('_rails_lite_app', @cookie)
   end
 end
