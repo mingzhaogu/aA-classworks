f8ecefff7df4c295af147b5e18f4c788ee2e5cb1
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-03-28 14:17:19 -0700

    Incorporate thunk middleware

--- W7D3/pokedex/frontend/pokedex.jsx
+++ W7D3/pokedex/frontend/pokedex.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
 
-import * as APIUtils from './util/api_util'; //
-import { receiveAllPokemon } from './actions/pokemon_actions' //
+// import * as APIUtils from './util/api_util'; //
+import { receiveAllPokemon, fetchAllPokemon, requestAllPokemon } from './actions/pokemon_actions' //
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  // window.getState = store.getState; //
-  // window.dispatch = store.dispatch; //
-  // window.fetchAllPokemon = APIUtils.fetchAllPokemon; //
-  // window.receiveAllPokemon = receiveAllPokemon; //
+  window.getState = store.getState; //
+  window.dispatch = store.dispatch; //
+  window.fetchAllPokemon = fetchAllPokemon; //
+  window.receiveAllPokemon = receiveAllPokemon; //
+  window.requestAllPokemon = requestAllPokemon; //
   ReactDOM.render(<h1>Pokedex</h1>, root);
 })
