df575fa8eb160547ac285505a891d11b2e2da47b
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-03-28 14:08:57 -0700

    Setup redux cycle, including store, actions, reducers [root, entities]

--- W7D3/pokedex/frontend/pokedex.jsx
+++ W7D3/pokedex/frontend/pokedex.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
+
+import * as APIUtils from './util/api_util'; //
+import { receiveAllPokemon } from './actions/pokemon_actions' //
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  // window.getState = store.getState; //
+  // window.dispatch = store.dispatch; //
+  // window.fetchAllPokemon = APIUtils.fetchAllPokemon; //
+  // window.receiveAllPokemon = receiveAllPokemon; //
   ReactDOM.render(<h1>Pokedex</h1>, root);
 })
---
+++ W7D3/pokedex/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+
+const pokemonReducer = (state = {}, action) => {
+  let newState;
+
+  switch (action.type) {
+  case RECEIVE_ALL_POKEMON:
+    newState = action.pokemon;
+    return newState;
+  default:
+    return state;
+  }
+};
+
+export default pokemonReducer;
